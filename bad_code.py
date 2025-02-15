67a52194c88498f3442d51c095d7f54c8b99b296
pavle0903 <60235682+pavle0903@users.noreply.github.com>
2025-02-15 17:12:15 +0100

    Update bad_code.py

--- bad_code.py
+++ bad_code.py
@@ -9,3 +9,10 @@ def execute_user_input():
     eval(user_input)  # 🚨 SonarQube will flag this as a security vulnerability
 
 execute_user_input()
+
+def execute_user_inpuut():
+    user_input = "print('This is insecure!')"  # Simulating user input
+    eval(user_input)  # 🚨 SonarQube will flag this as a security vulnerability
+
+execute_user_inpuut()
+
