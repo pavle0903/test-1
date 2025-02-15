4d55ae726e58214be2a8c26e99317da7ac78044e
pavle0903 <60235682+pavle0903@users.noreply.github.com>
2025-02-15 17:33:33 +0100

    Update bad_code.py

--- bad_code.py
+++ bad_code.py
@@ -1,18 +1,18 @@
-def bad_function():
-    print("This function has an unused variable")
-    unused_variable = 42  # SonarQube will flag this as a "Code Smell"
+# def bad_function():
+#     print("This function has an unused variable")
+#     unused_variable = 42  # SonarQube will flag this as a "Code Smell"
 
-bad_function()
+# bad_function()
 
-def execute_user_input():
-    user_input = "print('This is insecure!')"  # Simulating user input
-    eval(user_input)  # 🚨 SonarQube will flag this as a security vulnerability
+# def execute_user_input():
+#     user_input = "print('This is insecure!')"  # Simulating user input
+#     eval(user_input)  # 🚨 SonarQube will flag this as a security vulnerability
 
-execute_user_input()
+# execute_user_input()
 
-def execute_user_inpuut():
-    user_input = "print('This is insecure!')"  # Simulating user input
-    eval(user_input)  # 🚨 SonarQube will flag this as a security vulnerability
+# def execute_user_inpuut():
+#     user_input = "print('This is insecure!')"  # Simulating user input
+#     eval(user_input)  # 🚨 SonarQube will flag this as a security vulnerability
 
-execute_user_inpuut()
+# execute_user_inpuut()
 
